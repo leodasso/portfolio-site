88b4d5035be9fb5454059fdf609ec1420e048731
Leo Dasso <leo.dasso@gmail.com>
2019-04-04 22:39:58 -0500

    turn nav link into component, begin styling for mobile

---
+++ src/components/NavLink/NavLink.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './NavLink.css';
+
+class NavLink extends Component {
+    
+    render() {
+        return (
+            <Link to={this.props.url}>
+                <div className="nav-link">{this.props.children}</div>
+            </Link>
+        );
+    }
+}
+
+export default NavLink;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import './Profile.css';
-import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import NavLink from '../NavLink/NavLink';
 
 class Profile extends Component {
     
     render() {
         return (
             <div className="Profile">
-                <div className="profile-name">
-                    {/* These are placed in sep elements because I want the wrapping effect */}
-                    <h2 className="headline">LEO</h2>
-                    <h2 className="headline">DASSO</h2>
-                </div>
-                <div className="profile-info">
-                    <Link to="#">Projects</Link>
-                    <Link to="#">About</Link>
-                    <Link to="#">Contact</Link>
-                    <Link to="#">LinkedIn</Link>
-                    <Link to="#">GitHub </Link>
-                </div>
+                <Grid container spacing={32}>
+                    <Grid item sm={12} md={6}>
+                        <div className="profile-name">
+                            {/* These are placed in sep elements because I want the wrapping effect */}
+                            <h2 className="headline">LEO</h2>
+                            <h2 className="headline">DASSO</h2>
+                        </div>
+                    </Grid>
+                    <Grid item sm={12} md={6}>
+
+                        <div className="profile-info">
+                            <NavLink url="#">Projects</NavLink>
+                            <NavLink url="#">About</NavLink>
+                            <NavLink url="#">Contact</NavLink>
+                            <NavLink url="#">Resume</NavLink>
+                            <NavLink url="#">LinkedIn</NavLink>
+                            <NavLink url="#">GitHub</NavLink>
+
+                        </div>
+                    </Grid>
+
+                </Grid>
             </div>
         );
     }
