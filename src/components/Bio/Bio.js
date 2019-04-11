bc15aaca8863382538d7d6ed20f5e05f3da1b261
Leo Dasso <leo.dasso@gmail.com>
2019-04-11 10:35:45 -0500

    format bio for mobile

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -13,14 +13,24 @@ class Bio extends Component {
 			<>
 				
 			<Parallax
-				strength={300}
+				strength={400}
 			>
 				<div className="bio">
 					<img 
 						src={profilePic} 
 						className="profile-pic"/>
-					<p>
-						Hi, I'm Leo! I'm an American software developer with a background in design and games.
+					<p className="bio-text">
+						Hi, I'm Leo! 
+						<br/><br/>
+						I'm an American software developer with a background in design and games.
+						<br/><br/>
+						I've always been fascinated by computers. I got my first dive into programming in 
+						2002 when my middle school friend showed me that <em>you can actually write code in these graphinc calculators!</em>
+						<br/><br/>
+						In my six years of professional experience in game development, I learned that creating
+						a sustainable working habit is more important than squeezing every last drop of effort from myself
+						or my team. I beleive that most devs started off because of a fascination with the things that could
+						be accomplished with computers - it's important to treat this fascination with care.
 					</p>
 				</div>
 
